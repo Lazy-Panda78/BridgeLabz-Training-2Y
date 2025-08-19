11f8bf992252800370b16fcdf9bd1095dd1f6ae6
Lazy-Panda78 <yashupadhyaywork123@gmail.com>
2025-08-19 11:29:08 +0530

    Kms To miles

---
+++ KmsToMiles.java
@@ -0,0 +1,8 @@
+public class KmsToMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+    
+}
