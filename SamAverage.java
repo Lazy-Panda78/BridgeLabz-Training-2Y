0d74ac699c39e43162553db1ebecb6fd2fc872cd
Lazy-Panda78 <yashupadhyaywork123@gmail.com>
2025-08-19 11:22:44 +0530

    Sam's Average marks

---
+++ SamAverage.java
@@ -0,0 +1,11 @@
+public class SamAverage {
+    public static void main(String[] args) {
+        
+        int math= 94;
+        int phy= 95;
+        int chem= 96;
+        int avg = (math + phy + chem) / 3;
+        System.out.println("Average of Sam's marks is: " + avg);
+    }
+    
+}
